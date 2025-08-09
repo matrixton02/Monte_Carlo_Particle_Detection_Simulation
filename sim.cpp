cddd39b5759f3ef51862accde3f8f8f95822c568
matrixton02 <yashasvi21022005@gmail.com>
2025-08-10 05:17:58 +0530

    updated the random distribution system

--- sim.cpp
+++ sim.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <vector>
 #include <fstream>
-
+#include <chrono>
 using namespace std;
 
 double dt=1e-12;
@@ -64,8 +64,7 @@ class Particle{
         }
 
         void scatter(double angle_stddev){                                 //determines a random scattering value to simulate real life collisions
-            random_device rd;
-            mt19937 gen(rd());
+            mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
             normal_distribution<> d(0, angle_stddev);
         
             double theta = d(gen);
@@ -101,15 +100,13 @@ class Particle{
 };
 
 double angle_deviation(){                                           //to randomize the scattering of particles representing real life collisions
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> angle_dis(0.0,2*M_PI);
     return angle_dis(gen);
 }
 
 Vector random_direction() {                                         //generating random direction to create the initial starting conditions of the simulation
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> x_dis(0.5, 1.0);
     uniform_real_distribution<> y_dis(-0.1, 0.1);
     uniform_real_distribution<> z_dis(-0.1, 0.1);
@@ -123,18 +120,16 @@ Vector random_direction() {                                         //generating
 }
 
 Vector random_position(double boxSize = 100) {                      //generating random position to create the initial starting conditions of the simulation
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> disY(0.0, boxSize);
     uniform_real_distribution<> disZ(0.0, boxSize);
-    uniform_real_distribution<> disX(40.0, 60.0);                   // particles start around center
+    uniform_real_distribution<> disX(40.0, 75.0);                   // particles start around center
 
     return Vector(disX(gen), disY(gen), disZ(gen));
 }
 
 double random_Energy(double minE=1e-14,double maxE=1e-12){          //All particles are initially given random energy
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> dis(minE,maxE);
 
     return dis(gen);
@@ -147,10 +142,8 @@ Particle generate_random(){                                         // Genreates
         {"Piom",2.49e-28,1,2.6e-8}
     };
 
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0,types.size()-1);
-
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<> dis(0,2);
     ParticleType p=types[dis(gen)];
 
     Vector pos=random_position();
@@ -167,16 +160,14 @@ Particle generate_random(){                                         // Genreates
 }
 
 double random_dE(double energy){                                    //determines the radnom amount of energy that is reduced from the particle which signifies real life collsion
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> dis(0.01*energy,0.05*energy);       //the particle may lose anywhere arounf 1% to 5% energy
     double dE=dis(gen);
     return dE;
 }
 
 bool random_hit(double probability){                                // it returns wether the probbaility that we detected if higher than a threshold
-    random_device rd;
-    mt19937 gen(rd());
+    mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
     uniform_real_distribution<> dis(0.0,1.0);
 
     return dis(gen)<probability;
@@ -213,7 +204,7 @@ int main(){
     vector<Particle> particle;
     double i=0;
     int j=0;
-    for(i=0;i<10;i++){                                              //creating random particles for our simualtion
+    for(i=0;i<20;i++){                                              //creating random particles for our simualtion
         Particle p=generate_random();
         particle.push_back(p);
     }
@@ -221,20 +212,21 @@ int main(){
     for(i=0;i<1e-7;i+=dt){                                          //after every dt time we will run computations on all our particles
         for(j=0;j<particle.size();j++){
             Particle& p=particle[j];
+            //p.particle_stat();                                    //used to print the particle state at each point to check for any inconsistency or error
             if(p.is_alive(i)){                                      // if the particle is alive then only we will perform the computations
                 p.move(dt);                                         // we moved the particle
                 p.lose_energy(random_dE(p.energy));                 //energy is deducted
-                if(p.pos.x>=50 && p.pos.x<=52){                     //checking wether it is in the detector range or not
+                if((p.pos.x>=45 && p.pos.x<=50)){                     //checking wether it is in the detector range or not
                     if(random_hit(0.80)){
                         detected.push_back({"detector 1",p});
                     }
                 }
-                else if(p.pos.x>=53 && p.pos.x<=55){
+                else if((p.pos.x>=55 && p.pos.x<=60)){
                     if(random_hit(0.85)){
                         detected.push_back({"detector 2",p});
                     }
                 }
-                else if(p.pos.x>=56 && p.pos.x<=58){
+                else if((p.pos.x>=65 && p.pos.x<=70)){
                     if(random_hit(0.90)){
                         detected.push_back({"detector 3",p});
                     }
